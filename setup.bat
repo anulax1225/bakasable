132823f393051704984828afd1f5afd60a94c5a4
anulax1225 <126906518+anulax1225@users.noreply.github.com>
2024-06-06 20:39:12 +0200

    created setup.bat

---
+++ setup.bat
@@ -0,0 +1,10 @@
+@echo off
+
+echo Installing bakasable
+
+dir "C:\Program Files\bakasable"
+
+dir "C:\Program Files\bakasable\cache"
+
+copy .\bin\windows\bakasable.exe "C:\Program Files\bakasable\"
+
