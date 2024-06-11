f1a7f44fa095514a2d72c89ebe60573535342c87
anulax1225 <anulax1225@icloud.com>
2024-06-11 20:01:59 +0200

    Modified script.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 echo Installing bakasable
+
 handle_error() {
     echo "An error occurred on line $1"
     rm -rf ~/.bakasable
     exit 1
 }
 trap 'handle_error $LINENO' ERR
+
 mkdir -m 777 ~/.bakasable
 mkdir ~/.bakasable/cache
+
 cp ./bin/linux/bakasable ~/.bakasable/
+
 echo "export PATH=$PATH:~/.bakasable" >> ~/.bashrc
