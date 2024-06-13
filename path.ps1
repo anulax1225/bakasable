a0ad36ac93da9fe0f7cad2beff4a723ee4a6f833
Ambigapathy Vinayak <vinayak.ambigapathy@rpn.ch>
2024-06-13 09:49:13 +0200

    Added windows support but path not working

---
+++ path.ps1
@@ -0,0 +1,8 @@
+echo Searching path in env PATH
+$PATH = [Environment]::GetEnvironmentVariable("PATH", "Machine")
+$bakasable_path = "C:\Program Files\bakasable"
+if( $PATH -notlike "*"+$bakasable_path+"*" ){
+	echo Path not found in env PATH
+	echo Adding path
+    	[Environment]::SetEnvironmentVariable("PATH", "$PATH;$bakasable_path", "Machine")
+}
