2e8b96381d8d369ce52308e2182fae7896d9565f
Ambigapathy Vinayak <vinayak.ambigapathy@rpn.ch>
2024-06-13 09:48:52 +0200

    Added windows support but path not working

--- setup.bat
+++ setup.bat
@@ -6,7 +6,7 @@ md "C:\Program Files\bakasable"
 
 md "C:\Program Files\bakasable\cache"
 
-copy .\bin\windows\bakasable.exe "C:\Program Files\bakasable\"
+xcopy /y .\bin\windows\bakasable.exe "C:\Program Files\bakasable"
+xcopy /y .\bin\windows\premake5.exe "C:\Program Files\bakasable"
 
-setx /M path "%path%;C:\Program Files\bakasable\"
-
+powershell path.ps1
