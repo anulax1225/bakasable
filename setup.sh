cf4f218675083cd6a8f7c290d5a250956cdce368
anulax1225 <anulax1225@icloud.com>
2024-10-12 00:28:33 +0200

    Added compile commands module to premake for lsp intergration

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-echo Installing bakasable
-
 cd $(dirname "$0")
 
 handle_error() {
@@ -10,18 +8,24 @@ handle_error() {
 }
 trap 'handle_error $LINENO' ERR
 
+echo Installing bakasable
 mkdir -m 777 ~/.bakasable
 mkdir ~/.bakasable/cache
-
 cp -f ./bin/linux/bakasable ~/.bakasable/
+
 if [ ! $(which premake5) ]; then
+    echo Installing premake  
     cp -f ./bin/linux/premake5 ~/.bakasable/
+    echo Installing export-compile-commands module
+    cp -f ./bin/vendor/export-compile-commands ~/.bakasable/
+    echo 'require "export-compile-commands"' >> ~/.bakasable/
 fi
 
 echo Searching path in env PATH 
 if [ ! $(which bakasable) ]; then
     echo Path not found in env PATH
 	echo Adding path
+    export PATH=\$PATH:~/.bakasable
     echo "export PATH=\$PATH:~/.bakasable" >> ~/.bashrc
 else
     echo Path already added
